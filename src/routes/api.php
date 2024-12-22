bcae2665a8ca1656ac4768bc03c67cd75647d706
d.hasegawa <d.hasegawa@styleedge.co.jp>
2024-12-22 21:25:11 +0900

    認証機能の追加

--- src/routes/api.php
+++ src/routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// ログインと新規登録のルーティング
+Route::post('/register', 'App\Http\Controllers\AuthController@register');
+Route::post('/login', 'App\Http\Controllers\AuthController@login');
